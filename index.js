16d3eef17bf263d94550f720760c9fec7bfb2dbd
Salvatore Mazzarino <salvo.mazzarino@motorsquare.eu>
2015-05-06 21:36:19 +0200

    initialized the server

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var Hapi = require('hapi');
+
+var dotenv = require('dotenv');
+
+// load the env variable
+dotenv.load();
+
+var server = new Hapi.Server();
+
+server.connection({
+    
+    port: process.env.PORT
+});
+
+server.register([{
+
+    register: require('hapi-router'),
+    options: {
+        routes: 'routes/**/*.js'
+    }
+}], function(err) {
+
+    if (err) {
+        console.log('An error occured during the loading of a plugin');
+    }
+});
+
+server.start(function() {
+
+    console.log('Server running at', server.info.uri);
+});
+
+
