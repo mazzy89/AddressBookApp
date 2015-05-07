88d05fb1cc623607761175ff4c9e08e5c5a20ae0
Salvatore Mazzarino <salvo.mazzarino@motorsquare.eu>
2015-05-07 14:21:45 +0200

    Store contact information

--- handlers/contacts.js
+++ handlers/contacts.js
@@ -1,10 +1,34 @@
 'use strict'
 
+var Firebase = require('firebase');
+
+var env = process.env.NODE_ENV || 'development';
+var config = require(__dirname + '/../config/config.json')[env];
+
 module.exports = {
 
     createContact: function(request, reply) {
 
-        return reply('You are authenticated')
+        var accountId = request.auth.credentials.id;
+
+        // get data from
+        var firstName = request.payload.firstName;
+        var lastName = request.payload.lastName;
+        var phone = request.payload.phone;
+
+        //here save the data with firebase
+        var usersRef = new Firebase(config.firebaseUrl + '/users');
+        
+        usersRef
+            .child(accountId)
+            .child("contacts")
+            .push({
+                firstName: firstName,
+                lastName: lastName,
+                phone: phone
+            });
+        
+        return reply()
             .header("Authorization", request.headers.authorization)
             .code(201);
     }
