a0cdc28ff86c4b288e6e125241fd0cfbbf5f6c25
Salvatore Mazzarino <salvo.mazzarino@motorsquare.eu>
2015-05-10 21:37:44 +0200

    indexed email field

--- models/account.js
+++ models/account.js
@@ -2,16 +2,18 @@
 
 module.exports = function(sequelize, DataTypes) {
 
-  var Account = sequelize.define('Account', {
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
-  
-  return Account;
+    var Account = sequelize.define('Account', {
+        email: DataTypes.STRING,
+        password: DataTypes.STRING
+    }, {
+        indexes: [
+            // unique index on email
+            {
+                unique: true,
+                fields: ['email']
+            }
+        ]
+    });
+
+    return Account;
 };
